9de1ac0a57841e23147866a581178cec05c3e6e9
Gwanghyun Choi <66342017+helirang@users.noreply.github.com>
2022-03-06 22:38:34 +0900

    Update SameNumber.js

--- Lv1/SameNumber.js
+++ Lv1/SameNumber.js
@@ -1,3 +1,23 @@
+//내가 푼거
+function solutionOrigin(arr) {
+    var answer = [];
+    let tempArr = arr.slice();
+    let smallNum, smallIndex;
+
+    tempArr.sort((a, b)=>{ return a-b; });
+    smallNum = tempArr[0];
+    while(1){
+        smallIndex = arr.indexOf(smallNum);
+        if(smallIndex==-1) break;
+        arr.splice(smallIndex,1);
+    }
+    if(arr.length < 1) answer.push(-1)
+    else answer = arr;
+
+    return answer;
+}
+
+// 깔끔한 답안
 function solution(arr)
 {
     var answer = [];
